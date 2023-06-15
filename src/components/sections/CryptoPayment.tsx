9245e1e9914521f89ab861da27b3189314f448db
Tushar Saini <s2sharpit@gmail.com>
2023-06-15 17:04:01 +0530

    updating

--- src/components/sections/CryptoPayment.tsx
+++ src/components/sections/CryptoPayment.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 
 const data = [
   { p: "Upto 5% cashback on spends" },
-  { p: "Insured funds by BitGo post-swap." },
-  { p: "Integration with Apple / Samsung / Google Pay." },
+  { p: "Insured funds by BitGo post-swap" },
+  { p: "Integration with Apple / Samsung / Google Pay" },
   { p: "Top-up your crypto and spend in fiat" },
   { p: "Accepted at all Mastercard supported merchants" },
 ];
@@ -18,16 +18,18 @@ export default function CryptoPayment() {
         <Title className="font-extralight text-2xl">
           Debit Card powered by Mastercard
         </Title>
+        <div className="px-4">
         <ul className="list-disc pt- grid grid-cols-2 justify-center max-w-6xl gap-x-10 md:gap-x-40 items-center text-neutral-300 text-lg pl-4">
           {data.map((d, i) => (
             <li
-              key={i}
-              className="flexitems-center py-2 space-x-4 xl:py-3 max-w-[20rem] max-md:text-xs"
+            key={i}
+            className="flexitems-center py-2 space-x-4 xl:py-3 max-w-[20rem] max-md:text-xs"
             >
               {d.p}
             </li>
           ))}
         </ul>
+        </div>
       </div>
       <div className="absolute sm:hidden max-md:min-w-[100dvw] flex justify-self-center bottom-96 md:top-96">
         <Image
