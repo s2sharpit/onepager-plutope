842bd81cf4891620aecfe60e48bb4fec96fe31bd
Tushar Saini <s2sharpit@gmail.com>
2023-06-19 21:20:37 +0530

    fixing mt gap

--- src/components/sections/CryptoPayment.tsx
+++ src/components/sections/CryptoPayment.tsx
@@ -19,16 +19,16 @@ export default function CryptoPayment() {
           Debit Card powered by Mastercard
         </Title>
         <div className="px-4">
-        <ul className="list-disc pt- grid grid-cols-2 justify-center max-w-6xl gap-x-10 md:gap-x-40 items-center text-neutral-300 text-lg pl-4">
-          {data.map((d, i) => (
-            <li
-            key={i}
-            className="flexitems-center py-2 space-x-4 xl:py-3 max-w-[20rem] max-md:text-xs"
-            >
-              {d.p}
-            </li>
-          ))}
-        </ul>
+          <ul className="list-disc pt- grid grid-cols-2 justify-center max-w-6xl gap-x-10 md:gap-x-40 items-center text-neutral-300 text-lg pl-4">
+            {data.map((d, i) => (
+              <li
+                key={i}
+                className="flexitems-center py-2 space-x-4 xl:py-3 max-w-[20rem] max-md:text-xs"
+              >
+                {d.p}
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
       <div className="absolute sm:hidden max-md:min-w-[100dvw] flex justify-self-center bottom-96 md:top-96">
@@ -41,14 +41,14 @@ export default function CryptoPayment() {
         />
       </div>
       <Image
-        className="justify-self-center max-sm:mt-60 max-w-xl w-screen"
+        className="justify-self-center mt-60 sm:mt-12 max-w-xl w-screen"
         src={"/assets/Table.png"}
         width={900}
         height={900}
         alt=""
       />
       <Link
-        href={''}
+        href={""}
         target="_blank"
         className="pointer-events-none mt-12 text-center text-2xl md:text-3xl justify-self-center p-4 px-8 rounded-full bg-gradient-to-r from-purple-900 viapurple-700 to-blue-800"
       >
