38e017572d6580fa1269118b734c3497ca014f32
Tushar Saini <s2sharpit@gmail.com>
2023-06-22 10:12:00 +0530

    updated

--- src/components/sections/CryptoPayment.tsx
+++ src/components/sections/CryptoPayment.tsx
@@ -33,7 +33,7 @@ export default function CryptoPayment() {
       </div>
       <div className="absolute sm:hidden max-md:min-w-[100dvw] flex justify-self-center bottom-96 md:top-96">
         <Image
-          className="top-40 relative "
+          className="top-60 relative "
           src={"/assets/debitP.png"}
           width={1000}
           height={200}
@@ -51,9 +51,9 @@ export default function CryptoPayment() {
       </div>
       <Image
         className="justify-self-center mt-60 sm:mt-96 max-w-xl w-screen"
-        src={"/assets/Table.png"}
-        width={900}
-        height={900}
+        src={"/assets/tab.png"}
+        width={1000}
+        height={1000}
         alt=""
       />
       <Link
